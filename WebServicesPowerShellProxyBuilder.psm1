1eee2eac5400b2b96eca63d319b2264fab6f6715
ChrisMagnuson <hourglasssand@gmail.com>
2018-01-12 09:29:14 -0500

    Integrate code to work around bugs so that other functions don't have to account for edge case bugs

--- WebServicesPowerShellProxyBuilder.psm1
+++ WebServicesPowerShellProxyBuilder.psm1
@@ -159,13 +159,16 @@ function ConvertFrom-PSBoundParameters {
     )
     process {        
         $Object = [pscustomobject]([ordered]@{}+$ValueFromPipeline) |
-        Select-Object -Property $Property -ExcludeProperty $ExcludeProperty
+        Select-Object -Property $Property -ExcludeProperty $ExcludeProperty |
+        Where-Object {$_.psobject.Properties.name -ne "*"} # There is a bug in PowerShell <v6 that returns a property named * when all the properites of the object are excluded
+        #https://github.com/PowerShell/PowerShell/issues/2351
 
         if ($AsHashTable) {
-            $Object | ConvertTo-HashTable
+            $Object | ConvertTo-HashTable -Ordered
         } else {
             $Object
         }
+
     }
 }
 
