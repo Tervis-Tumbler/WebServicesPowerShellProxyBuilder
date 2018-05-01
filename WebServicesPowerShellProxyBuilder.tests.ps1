9f40b8196f3eacd71423d53359ecb37be608fafe
ChrisMagnuson <hourglasssand@gmail.com>
2018-05-01 10:36:36 -0400

    Initial start of trying to test

---
+++ WebServicesPowerShellProxyBuilder.tests.ps1
@@ -0,0 +1,22 @@
+﻿Describe "ConvertFrom-PSBoundParameters" {
+    function Test-Function {
+        param (
+            $Thing1,
+            $Thing2,
+            $Thing3,
+            $ScriptBlock
+        )
+        & $ScriptBlock $PSBoundParameters
+    }
+
+    It "Shouldn't include properties with no value" {
+        $ScriptBlock = { 
+            param (
+                [Parameter(Mandatory,ValueFromPipeline)]$ValueFromPipeline
+            ) 
+            $ValueFromPipeline | ConvertFrom-PSBoundParameters
+        }
+
+        $Result = Test-Function -Thing1 Value1 -ScriptBlock $ScriptBlock
+    }
+}
