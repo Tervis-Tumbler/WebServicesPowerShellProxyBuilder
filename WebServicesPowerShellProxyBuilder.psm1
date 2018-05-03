8f344871349b565c7e270b74bdfef9df032348f8
ChrisMagnuson <hourglasssand@gmail.com>
2018-05-03 11:50:34 -0400

    Fix regression, revert select-object statement back to the way it was

--- WebServicesPowerShellProxyBuilder.psm1
+++ WebServicesPowerShellProxyBuilder.psm1
@@ -166,7 +166,7 @@ function ConvertFrom-PSBoundParameters {
         
         if ($PropertiesToInclude) {
             $Object = [pscustomobject]([ordered]@{}+$ValueFromPipeline) |
-            Select-Object @SelectObjectParameters |
+            Select-Object -Property $PropertiesToInclude -ExcludeProperty $ExcludeProperty |
             Where-Object {$_.psobject.Properties.name -ne "*"} # There is a bug in PowerShell <v6 that returns a property named * when all the properites of the object are excluded
             #https://github.com/PowerShell/PowerShell/issues/2351
         }
